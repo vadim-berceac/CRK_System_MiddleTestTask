f0daf256f0ee0aaa167e08732ab33432b4fca7d9
v.berchak <v.berchak@nekki.com>
2026-05-23 16:57:24 +0300

    fixes

--- Assets/Scripts/UI/BreedDetailsPopup.cs
+++ Assets/Scripts/UI/BreedDetailsPopup.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using Zenject;
 
 public class BreedDetailsPopup : MonoBehaviour
@@ -9,7 +10,7 @@ public class BreedDetailsPopup : MonoBehaviour
     [SerializeField] private TextMeshProUGUI breedNameText;
     [SerializeField] private TextMeshProUGUI breedDescriptionText;
     [SerializeField] private Button closeButton;
-    [SerializeField] private LayoutElement layoutElement;
+    [SerializeField] private RectTransform popupRect;
     
     [Inject] private readonly BreedsPollingScheduler _scheduler;
     [Inject] private readonly LoadingIndicator _loadingIndicator;
@@ -41,7 +42,6 @@ public class BreedDetailsPopup : MonoBehaviour
     private void OnBreedDetailsLoaded(BreedDetailsData details)
     {
         _loadingIndicator.Hide();
-        
         DisplayBreedDetails(details);
     }
     
@@ -49,15 +49,24 @@ public class BreedDetailsPopup : MonoBehaviour
     {
         breedNameText.text = details.Name ?? "Неизвестная порода";
         breedDescriptionText.text = details.Description ?? "Описание отсутствует";
-    
-        AdjustPopupHeight();
-    
+        
         popupPanel.SetActive(true);
+        StartCoroutine(AdjustPopupHeightRoutine());
     }
     
-    private void AdjustPopupHeight()
+    private IEnumerator AdjustPopupHeightRoutine()
     {
-        layoutElement.preferredHeight = 400f;
+        yield return null;
+        yield return null;
+    
+        breedNameText.ForceMeshUpdate();
+        breedDescriptionText.ForceMeshUpdate();
+    
+        var titleHeight = breedNameText.preferredHeight;
+        var descriptionHeight = breedDescriptionText.preferredHeight;
+        var totalHeight = titleHeight + descriptionHeight + 60f;
+    
+        popupRect.sizeDelta = new Vector2(popupRect.sizeDelta.x, totalHeight);
     }
     
     private void ClosePopup()
